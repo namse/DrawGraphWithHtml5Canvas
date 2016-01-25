9d1825704eaa4dccc70cdfe458fcd5734ccc1bb6
skatpgusskat <skatpgusskat@naver.com>
2016-01-25 21:26:27 +0900

    sync

--- js/canvas.js
+++ js/canvas.js
@@ -1,84 +1,86 @@
 /* global Node */
 function Canvas(canvasDOM) {
-    canvasDOM = canvasDOM;
-    this.drawableChildren = [];
-    var CanvasState = {
-        IDLE: 0,
-        NODE_CLICKED: 1,
-        DRAWING_LINE_POINT_CLICKED: 2
-    };
-    var canvasStateMachine = CanvasState.IDLE;
-    var mouseX;
-    var mouseY;
-    var focusedNode;
+	canvasDOM = canvasDOM;
+	this.drawableChildren = [];
+	var CanvasState = {
+		IDLE: 0,
+		NODE_CLICKED: 1,
+		DRAWING_LINE_POINT_CLICKED: 2
+	};
+	var canvasStateMachine = CanvasState.IDLE;
+	var mouseX;
+	var mouseY;
+	var clickedNode;
+	var clickedNodeDrawingLinePointDirection;
 
-    this.addNode = function (image, x, y, width, height) {
-        if (typeof x === 'undefined') {
-            var position = this.getCurrentMousePosition();
-            x = position.x;
-            y = position.y;
-        }
-        var node = new Node(image, x, y, width, height);
-        this.drawableChildren.push(node);
-    };
+	this.addNode = function(image, x, y, width, height) {
+		if (typeof x === 'undefined') {
+			var position = this.getCurrentMousePosition();
+			x = position.x;
+			y = position.y;
+		}
+		var node = new Node(image, x, y, width, height);
+		this.drawableChildren.push(node);
+	};
 
-    this.onRender = function () {
-        var ctx = canvasDOM.getContext('2d');
-        if (!!!ctx) {
-            console.log("can't get context of Canvas DOM.");
-        }
-        else {
-            for (var i in this.drawableChildren) {
-                this.drawableChildren[i].onRender(ctx);
-            }
-        }
-    };
+	this.onRender = function() {
+		var ctx = canvasDOM.getContext('2d');
+		if (!!!ctx) {
+			console.log("can't get context of Canvas DOM.");
+		} else {
+			for (var i in this.drawableChildren) {
+				this.drawableChildren[i].onRender(ctx);
+			}
+		}
+	};
 
 
-    // return { x, y }
-    this.getCurrentMousePosition = function () {
+	// return { x, y }
+	this.getCurrentMousePosition = function() {
 
-        // todo
+		// todo
 
-        return {
-            x: 0,
-            y: 0
-        };
-    };
+		return {
+			x: 0,
+			y: 0
+		};
+	};
 
-    $(canvasDOM).mousedown(function (e) {
-        updateMousePosition(e);
-        if (canvasStateMachine === CanvasState.IDLE) {
-            for (var i in this.drawableChildren) {
-                if (this.drawableChildren[i] instanceof Node) {
-                    var node = this.drawableChildren[i];
-                    if (node.isInBound(mouseX, mouseY)) {
-                        focusedNode.isShowDrawingLinePoint = false;
-                        focusedNode = node;
-                        node.isShowDrawingLinePoint = true;
-                        
-                        if (node.isInDrawingLinePointBound(mouseX, mouseY)) {
-                            canvasStateMachine = CanvasState.DRAWING_LINE_POINT_CLICKED;
-                        }
-                        else {
-                            canvasStateMachine.CanvasState.NODE_CLICKED;
-                        }
-                        break;
-                    }
-                }
-            }
-        }
-    }).mouseup(function (e) {
-        updateMousePosition(e);
-    }).mousemove(function (e) {
-        updateMousePosition(e);
-    }).mouseenter(function (e) {
-        updateMousePosition(e);
-    });
+	$(canvasDOM).mousedown(function(e) {
+		updateMousePosition(e);
+		if (canvasStateMachine === CanvasState.IDLE) {
+			for (var i in this.drawableChildren) {
+				if (this.drawableChildren[i] instanceof Node) {
+					var node = this.drawableChildren[i];
+					if (node.isInBound(mouseX, mouseY)) {
+						if (clickedNode) {
+							clickedNode.isShowDrawingLinePoint = false;
+						}
+						clickedNode = node;
+						node.isShowDrawingLinePoint = true;
 
-    function updateMousePosition(e) {
-        mouseX = e.pageX - canvasDOM.offsetLeft;
-        mouseY = e.pageY - canvasDOM.offsetTop;
-        console.log(mouseX + ' ' + mouseY);
-    }
+						clickedNodeDrawingLinePointDirection = node.isInDrawingLinePointBound(mouseX, mouseY); // false of Direction
+						if (clickedNodeDrawingLinePointDirection) {
+							canvasStateMachine = CanvasState.DRAWING_LINE_POINT_CLICKED;
+						} else {
+							canvasStateMachine.CanvasState.NODE_CLICKED;
+						}
+						break;
+					}
+				}
+			}
+		}
+	}).mouseup(function(e) {
+		updateMousePosition(e);
+	}).mousemove(function(e) {
+		updateMousePosition(e);
+	}).mouseenter(function(e) {
+		updateMousePosition(e);
+	}).mouseleave;
+
+	function updateMousePosition(e) {
+		mouseX = e.pageX - canvasDOM.offsetLeft;
+		mouseY = e.pageY - canvasDOM.offsetTop;
+		console.log(mouseX + ' ' + mouseY);
+	}
 }
--- js/node.js
+++ js/node.js
@@ -1,112 +1,104 @@
 'use strict';
 
-function Node(image, x, y, width, height){
-    // constructor
-    this.image = image;
-    this.x = x;
-    this.y = y;
-    this.width = width || image.width;
-    this.height = width || image.height;
+function Node(image, x, y, width, height) {
+	// constructor
+	this.image = image;
+	this.x = x;
+	this.y = y;
+	this.width = width || image.width;
+	this.height = width || image.height;
 
-    this.lines = [];
-    this.isShowDrawingLinePoint = false;
-    this.highlightedDrawingLinePointDirection = undefined; // or DrawingLinePointDirection
-    
+	this.lines = [];
+	this.isShowDrawingLinePoint = false;
+	this.highlightedDrawingLinePointDirection = undefined; // or DrawingLinePointDirection
 
-    const DrawingLinePointRadius = 4;
-    const DrawingLinePointDirection = {
-        UP : 'up',
-        DOWN : 'down',
-        LEFT : 'left',
-        RIGHT : 'right'
-    };
-    
-    this.onRender = function(ctx){
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-        for(var i in this.lines){
-            this.lines[i].onRender();
-        }
-        if(this.showDrawingLinePoint){
-            for(var direction in DrawingLinePointDirection){
-                var position = this.getDrawingLinePointPosition(DrawingLinePointDirection[direction]);
-                ctx.beginPath();
-                ctx.arc(position.x, position.y, DrawingLinePointRadius, 0, 2 * Math.PI);
-                ctx.fillStyle = "red";
-                ctx.fill();
-                if(this.highlightedDrawingLinePointDirection == direction){
-                    ctx.strokeStyle = "black";
-                    ctx.lineWidth = 1;
-                    ctx.stroke();
-                }
-            }
-        }
-    };
 
-    this.isInBound = function(x, y){
-        if(this.x >= x && this.x + this.width <= x
-        && this.y >= y && this.y + this.height <= y){
-            return true;
-        }
-        return false;
-    };
-    
-    this.isInDrawingLinePointBound = function(x, y){
-        if(this.isInBound(x, y)){
-            var radius = DrawingLinePointRadius;
-            
-            // left or right
-            if(this.y + (this.height)/2 - radius >= y 
-            && this.y + (this.height)/2 + radius <= y){
-                if(this.x >= x 
-                && this.x + 2 * radius <= x){
-                    return DrawingLinePointDirection.LEFT;
-                }
-                else if(this.x + this.width - 2 * radius >= x 
-                && this.x + this.width <= x){
-                    return DrawingLinePointDirection.RIGHT;
-                }
-            }
-            // up or down
-            else if(this.x + (this.width)/2 - radius >= x 
-            && this.x + (this.width)/2 + radius <= x){
-                // up
-                if(this.y >= y 
-                && this.y + 2 * radius <= y){
-                    return DrawingLinePointDirection.UP;
-                }
-                // down
-                else if(this.y + this.height - 2 * radius >= y
-                && this.y + this.height <= y){
-                    return DrawingLinePointDirection.DOWN;
-                }
-            }
-        }
-        return false;
-    };
-    
-    this.getDrawingLinePointPosition = function(drawingLinePointDirection){
-        var radius = DrawingLinePointRadius;
-        if(drawingLinePointDirection === DrawingLinePointDirection.LEFT){
-            return {
-                x: this.x + radius,
-                y: this.y + (this.height)/2
-            };
-        } else if(drawingLinePointDirection === DrawingLinePointDirection.RIGHT){
-            return {
-                x: this.x + this.width - radius,
-                y: this.y + (this.height)/2
-            }
-        } else if(drawingLinePointDirection === DrawingLinePointDirection.UP){
-            return {
-                x: this.x + (this.width)/2,
-                y: this.y + radius
-            }
-        } else if(drawingLinePointDirection === DrawingLinePointDirection.DOWN){
-            return {
-                x: this.x + (this.width)/2,
-                y: this.y + this.height - radius
-            }
-        }
-        return undefined;
-    }
+	const DrawingLinePointRadius = 2;
+	const DrawingLinePointDirection = {
+		UP: 'up',
+		DOWN: 'down',
+		LEFT: 'left',
+		RIGHT: 'right'
+	};
+
+	this.onRender = function(ctx) {
+		ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+		for (var i in this.lines) {
+			this.lines[i].onRender();
+		}
+		if (this.showDrawingLinePoint) {
+			for (var direction in DrawingLinePointDirection) {
+				var position = this.getDrawingLinePointPosition(DrawingLinePointDirection[direction]);
+				ctx.beginPath();
+				ctx.arc(position.x, position.y, DrawingLinePointRadius, 0, 2 * Math.PI);
+				ctx.fillStyle = "red";
+				ctx.fill();
+				if (this.highlightedDrawingLinePointDirection == direction) {
+					ctx.strokeStyle = "black";
+					ctx.lineWidth = 1;
+					ctx.stroke();
+				}
+			}
+		}
+	};
+
+	this.isInBound = function(x, y) {
+		if (this.x >= x && this.x + this.width <= x && this.y >= y && this.y + this.height <= y) {
+			return true;
+		}
+		return false;
+	};
+
+	this.isInDrawingLinePointBound = function(x, y) {
+		if (this.isInBound(x, y)) {
+			var radius = DrawingLinePointRadius;
+
+			// left or right
+			if (this.y + (this.height) / 2 - radius >= y && this.y + (this.height) / 2 + radius <= y) {
+				if (this.x >= x && this.x + 2 * radius <= x) {
+					return DrawingLinePointDirection.LEFT;
+				} else if (this.x + this.width - 2 * radius >= x && this.x + this.width <= x) {
+					return DrawingLinePointDirection.RIGHT;
+				}
+			}
+			// up or down
+			else if (this.x + (this.width) / 2 - radius >= x && this.x + (this.width) / 2 + radius <= x) {
+				// up
+				if (this.y >= y && this.y + 2 * radius <= y) {
+					return DrawingLinePointDirection.UP;
+				}
+				// down
+				else if (this.y + this.height - 2 * radius >= y && this.y + this.height <= y) {
+					return DrawingLinePointDirection.DOWN;
+				}
+			}
+		}
+		return false;
+	};
+
+	this.getDrawingLinePointPosition = function(drawingLinePointDirection) {
+		var radius = DrawingLinePointRadius;
+		if (drawingLinePointDirection === DrawingLinePointDirection.LEFT) {
+			return {
+				x: this.x + radius,
+				y: this.y + (this.height) / 2
+			};
+		} else if (drawingLinePointDirection === DrawingLinePointDirection.RIGHT) {
+			return {
+				x: this.x + this.width - radius,
+				y: this.y + (this.height) / 2
+			}
+		} else if (drawingLinePointDirection === DrawingLinePointDirection.UP) {
+			return {
+				x: this.x + (this.width) / 2,
+				y: this.y + radius
+			}
+		} else if (drawingLinePointDirection === DrawingLinePointDirection.DOWN) {
+			return {
+				x: this.x + (this.width) / 2,
+				y: this.y + this.height - radius
+			}
+		}
+		return undefined;
+	}
 }
